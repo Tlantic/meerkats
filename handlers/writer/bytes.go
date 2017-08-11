513203c5a513ec41c30429a39b81a6166f852c31
João Pinho <joaolpinho@gmail.com>
2017-08-11 09:57:02 +0100

    fix(writer) dedupe fields

--- handlers/writer/bytes.go
+++ handlers/writer/bytes.go
@@ -2,26 +2,26 @@ package writer
 
 import "strconv"
 
-func appendBool(content []byte, key string, value bool) []byte {
-	return strconv.AppendBool(append(append(append(content, ' '), key...), '='), value)
+func appendBool(key string, value bool) []byte {
+	return strconv.AppendBool(append(append([]byte(key), '=')), value)
 }
 
-func appendString(content []byte, key string, value string) []byte {
-	return append(append(append(append(append(content, ' '), key...), []byte("=\"")...), value...), '"')
+func appendString(key string, value string) []byte {
+	return append(append(append([]byte(key), []byte("=\"")...), value...), '"')
 }
 
-func appendInt64(content []byte, key string, value int64) []byte {
-	return strconv.AppendInt(append(append(append(append(content, ' '), key...), '=')), value, 10)
+func appendInt64(key string, value int64) []byte {
+	return strconv.AppendInt(append(append([]byte(key), '=')), value, 10)
 }
 
-func appendUint64(content []byte, key string, value uint64) []byte {
-	return strconv.AppendUint(append(append(append(content, ' '), key...), '='), value, 10)
+func appendUint64(key string, value uint64) []byte {
+	return strconv.AppendUint(append(append([]byte(key), '=')), value, 10)
 }
 
-func appendFloat32(content []byte, key string, value float32) []byte {
-	return strconv.AppendFloat(append(append(append(append(content, ' '), key...), '=')), float64(value), 'f', -1, 32)
+func appendFloat32(key string, value float32) []byte {
+	return strconv.AppendFloat(append(append([]byte(key), '=')), float64(value), 'f', -1, 32)
 }
 
-func appendFloat64(content []byte, key string, value float64) []byte {
-	return strconv.AppendFloat(append(append(append(append(content, ' '), key...), '=')), value, 'f', -1, 64)
+func appendFloat64(key string, value float64) []byte {
+	return strconv.AppendFloat(append(append([]byte(key), '=')), value, 'f', -1, 64)
 }
